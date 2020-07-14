ee6ef3f46d515c14b6eaaee66a8582c1e60cd32c
n01508 <n01508@cybird-53856.local>
2020-07-14 14:56:45 +0900

    fix

--- cypress/integration/tst_spec.js
+++ cypress/integration/tst_spec.js
@@ -1,7 +1,7 @@
 describe("Google Test", () => {
   it('Shows Google', () => {
-    cy.visit('https://www.google.com/')
+    cy.visit('http://localhost:3000')
 
-    cy.title().should("include", "Google")
+    cy.get("#js-game-start-btn", {timeout:20000}).should("be.visible")
   })
 })
