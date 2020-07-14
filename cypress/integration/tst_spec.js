144980d0a824648b0863eeb52d9e09b6662a0efa
n01508 <n01508@cybird-53856.local>
2020-07-14 15:18:11 +0900

    fix

--- cypress/integration/tst_spec.js
+++ cypress/integration/tst_spec.js
@@ -2,6 +2,9 @@ describe("Google Test", () => {
   it('Shows Google', () => {
     cy.visit('http://localhost:3000')
 
+    window.localStorage.setItem('iv', "abcdABCD12345678")
+    window.localStorage.setItem('uuid', "nAgtjfnkhfxSQZ/TpPWAjQ==")
+    cy.get("body", {timeout:20000}).should("be.visible")
     cy.get("#js-game-start-btn", {timeout:20000}).should("be.visible")
   })
 })
